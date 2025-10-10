c9f18c7968b4548cdd2be1e6066f845d6e7c7c3e
alexanderkokona <alexanderkokona@gmail.com>
2025-10-09 20:57:27 -0600

    team 4

--- lesson_04/team/team.py
+++ lesson_04/team/team.py
@@ -1,11 +1,13 @@
-""" 
+"""
 Course: CSE 351
 Team  : Week 04
 File  : team.py
-Author: <Student Name>
+Author: Alexander Kokona
 
-See instructions in canvas for this team activity.
-
+Purpose:
+Use semaphores to synchronize producers and consumers.
+Each producer generates random numbers and places them in a shared queue.
+Consumers remove numbers, check for primes, and write primes to a file.
 """
 
 import random
@@ -13,6 +15,7 @@ import threading
 
 # Include CSE 351 common Python files. 
 from cse351 import *
+from matplotlib.pyplot import bar
 
 # Constants
 MAX_QUEUE_SIZE = 10
@@ -56,42 +59,90 @@ class Queue351():
         return len(self.__items) + extra
 
 # ---------------------------------------------------------------------------
-def producer():
+def producer(id, que, empty_slots, full_slots, barrier):
     for i in range(PRIME_COUNT):
         number = random.randint(1, 1_000_000_000_000)
-        # TODO - place on queue for workers
+
+        # must acquire an empty slot before putting
+        empty_slots.acquire()
+        que.put(number)
+
+        # must release a full slot after putting
+        full_slots.release()
+
+    # wait for all producers to finish
+    barrier.wait()
 
     # TODO - select one producer to send the "All Done" message
+    if id == 0:
+        for _ in range(CONSUMERS):
+            empty_slots.acquire()
+            que.put(None)
+            full_slots.release()
 
 # ---------------------------------------------------------------------------
-def consumer():
-    # TODO - get values from the queue and check if they are prime
-    # TODO - if prime, write to the file
-    # TODO - if "All Done" message, exit the loop
-    ...
+def consumer(que, empty_slots, full_slots, filename):
+    while True:
+        # must acquire a full slot before getting
+        full_slots.acquire()
+        number = que.get()
+
+        # must release an empty slot after getting
+        empty_slots.release()
+
+        if number is None:
+            break
+
+        if is_prime(number):
+            print(f"Found prime: {number}")
+            with open(filename, 'a') as f:
+                f.write(f"{number}\n")
 
 # ---------------------------------------------------------------------------
 def main():
 
     random.seed(102030)
 
+    # clear the file
+    with open(FILENAME, 'w') as f:
+        ...  
+
     que = Queue351()
 
-    # TODO - create semaphores for the queue (see Queue351 class)
+    # create semaphores for the queue (see Queue351 class)
+    # Note: the values of the two semaphores MUST ALWAYS be MAX_QUEUE_SIZE
+    empty_slots = threading.Semaphore(MAX_QUEUE_SIZE)
+    full_slots = threading.Semaphore(0)
 
-    # TODO - create barrier
+    # create barrier
+    barrier = threading.Barrier(PRODUCERS)
 
-    # TODO - create producers threads (see PRODUCERS value)
+    # create producers threads (see PRODUCERS value)
+    producers = []
+    for i in range(PRODUCERS):
+        p = threading.Thread(target=producer, args=(i, que, empty_slots, full_slots, barrier))
+        p.start()
+        producers.append(p)
 
-    # TODO - create consumers threads (see CONSUMERS value)
+    # create consumers threads (see CONSUMERS value)
+    consumers = []
+    for i in range(CONSUMERS):
+        c = threading.Thread(target=consumer, args=(que, empty_slots, full_slots, FILENAME))
+        c.start()
+        consumers.append(c)
+
+    # wait for all to finish
+    for t in producers + consumers:
+        t.join()
+
+    # Show the number of primes found
 
     if os.path.exists(FILENAME):
         with open(FILENAME, 'r') as f:
             primes = len(f.readlines())
     else:
         primes = 0
-    print(f"Found {primes} primes. Must be 108 found.")
-
+    print(f"Found {primes} primes.  Must be 108 found.")
 
 
 if __name__ == '__main__':
